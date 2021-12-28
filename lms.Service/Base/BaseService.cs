9bdcae7169fd2e735261a11de6b17d6540083c07
shohag06 <shohag.cse06@gmail.com>
2021-12-28 06:49:34 -0800

    addBaseService

---
+++ lms.Service/Base/BaseService.cs
@@ -0,0 +1,45 @@
+﻿using lms.Repository.Contracts;
+using lms.Service.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lms.Service.Base
+{
+    public class BaseService<T>: IBaseService<T> where T:class
+    {
+
+        private IRepository<T> _repository;
+
+        public BaseService(IRepository<T> repository)
+        {
+            _repository = repository;
+        }
+
+
+        public virtual bool Add(T entity)
+        {
+            return _repository.Add(entity);
+        }
+
+        public virtual bool Update(T entity)
+        {
+            return _repository.Update(entity);
+        }
+
+        public virtual bool Remove(T entity)
+        {
+            return _repository.Remove(entity);
+        }
+
+        public virtual T GetById(int id)
+        {
+            return _repository.GetById(id);
+        }
+
+        public virtual ICollection<T> GetAll()
+        {
+            return _repository.GetAll();
+        }
+    }
+}
---
+++ lms.Service/Contracts/IBaseService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lms.Service.Contracts
+{
+    public interface IBaseService<T> where T : class
+    {
+        bool Add(T entity);
+
+
+        bool Update(T entity);
+        bool Remove(T entity);
+        T GetById(int id);
+        ICollection<T> GetAll();
+    }
+}
