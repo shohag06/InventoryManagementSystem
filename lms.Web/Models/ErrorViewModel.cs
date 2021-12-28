19edec92c0e3a5976974e6de13667800fd2404a1
shohag06 <shohag.cse06@gmail.com>
2021-12-28 05:18:29 -0800

    LayeredAdded

---
+++ lms.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace lms.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
