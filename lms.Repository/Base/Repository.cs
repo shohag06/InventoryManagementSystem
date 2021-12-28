81da49ff15e70dbd040ca10647b0ae73e6bb271d
shohag06 <shohag.cse06@gmail.com>
2021-12-28 06:30:49 -0800

    baseRepositoryAdded

---
+++ lms.Repository/Base/Repository.cs
@@ -0,0 +1,56 @@
+﻿using Ims.Data;
+using lms.Repository.Contracts;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace lms.Repository.Base
+{
+    public class Repository<T>:IRepository<T> where T : class
+    {
+
+        private readonly ApplicationDbContext _db;
+        public Repository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        private DbSet<T> Table
+        {
+            get { return _db.Set<T>(); }
+        }
+
+        public virtual bool Add(T entity)
+        {
+            
+            
+            Table.Add(entity);
+            return _db.SaveChanges() > 0;
+        }
+
+        public virtual bool Update(T entity)
+        {
+            _db.Entry(entity).State = EntityState.Modified;
+            return _db.SaveChanges() > 0;
+        }
+
+        public virtual bool Remove(T entity)
+        {
+            Table.Remove(entity);
+            return _db.SaveChanges() > 0;
+        }
+
+        public virtual T GetById(int id)
+        {
+            return Table.Find(id);
+        }
+
+        public virtual ICollection<T> GetAll()
+        {
+            return Table.ToList();
+        }
+
+    }
+}
---
+++ lms.Repository/Contracts/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lms.Repository.Contracts
+{
+    public interface IRepository<T> where T:class
+    {
+        bool Add(T entity);
+        bool Update(T entity);
+        bool Remove(T enetity);
+        T GetById(int id);
+        ICollection<T> GetAll();
+    }
+}
