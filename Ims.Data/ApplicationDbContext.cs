9f5281c3e6515cbee0219e30f3b6f0633f1c557e
shohag06 <shohag.cse06@gmail.com>
2021-12-28 05:50:09 -0800

    addedCustomerModel&ConnectDatabase

---
+++ Ims.Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using lms.Model;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ims.Data
+{
+    public class ApplicationDbContext: IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+           : base(options)
+        {
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
