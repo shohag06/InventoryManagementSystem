604ca8b236318117d26b028ee25bbeb0128afbec
shohag06 <shohag.cse06@gmail.com>
2021-12-29 10:21:52 -0800

    OrderViewModelAdded

---
+++ lms.Web/Models/OrderVM/OrderViewModel.cs
@@ -0,0 +1,27 @@
+﻿using lms.Model;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace lms.Web.Models.OrderVM
+{
+    public class OrderViewModel
+    {
+        public int Id { get; set; }
+        [Display(Name = "Order No")]
+        public string OrderNo { get; set; }
+        [Display(Name = "Date")]
+        public DateTime OrderDate { get; set; }
+
+        [Display(Name = "Customer")]
+        public int? CustomerId { get; set; }
+        public int? ProductId { get; set; }
+        public List<OrderDetail> OrderDetails { get; set; }
+
+        public List<SelectListItem> ProductSelectItems { get; set; }
+        public List<SelectListItem> Customers { get; set; }
+    }
+}
